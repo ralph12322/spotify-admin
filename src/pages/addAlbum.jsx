652ec59f9021099dd0aa220d76611291450e580b
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 14:04:14 +0800

    d

--- src/pages/addAlbum.jsx
+++ src/pages/addAlbum.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { assets } from "../assets/assets";
 import { toast } from "react-toastify";
 import { url } from "../config";
 import axios from "axios";
--- src/pages/addSong.jsx
+++ src/pages/addSong.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { assets } from "../../../Spotify-Clone/src/assets/assets";
+import { assets } from "../assets/assets";
 import axios from 'axios'
 import { toast } from "react-toastify";
 import { url } from "../config";
